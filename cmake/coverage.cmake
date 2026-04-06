9a2cb2e072895dc92c4951095a8f08f73b266d1e
royyandzakiy <royyan.abdul@gmail.com>
2026-04-06 13:06:03 +0700

    fix: coverage folder where binary is in

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -42,21 +42,6 @@ if(ENABLE_COVERAGE)
             COMMENT "Generating Clang coverage report with llvm-cov"
         )
         
-        # Optional: LCOV-compatible target (if you need LCOV format)
-        add_custom_target(coverage-lcov
-            # Run test to generate .gcda files
-            COMMAND ${CMAKE_BINARY_DIR}/test
-            # Use lcov with gcov wrapper
-            COMMAND ${LCOV} --gcov-tool ${GCOV_WRAPPER} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info --ignore-errors gcov,graph,version
-            # Remove system files
-            COMMAND ${LCOV} --remove ${CMAKE_BINARY_DIR}/coverage.info '/usr/*' --output-file ${CMAKE_BINARY_DIR}/coverage.info --ignore-errors unused,version
-            # Generate HTML report
-            COMMAND ${GENHTML} --demangle-cpp -o ${CMAKE_BINARY_DIR}/coverage_report_lcov ${CMAKE_BINARY_DIR}/coverage.info
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            DEPENDS test
-            COMMENT "Generating Clang coverage report with LCOV (alternative)"
-        )
-        
     else()
         # GCC coverage setup
         message(STATUS "Setting up GCC coverage with gcov/lcov")
@@ -64,16 +49,16 @@ if(ENABLE_COVERAGE)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 --coverage")
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 --coverage")
         
-        # Add coverage target for GCC
+        # Add coverage target for GCC with correct base directory
         add_custom_target(coverage
             # Run test to generate .gcda files
             COMMAND ${CMAKE_BINARY_DIR}/test
-            # Capture coverage data
-            COMMAND ${LCOV} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info --ignore-errors gcov,graph,version
-            # Remove system files
-            COMMAND ${LCOV} --remove ${CMAKE_BINARY_DIR}/coverage.info '/usr/*' --output-file ${CMAKE_BINARY_DIR}/coverage.info --ignore-errors unused,version
+            # Capture coverage data - use --base-directory to point to source root
+            COMMAND ${LCOV} --directory ${CMAKE_BINARY_DIR} --base-directory ${CMAKE_SOURCE_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info --ignore-errors gcov,graph,version,source
+            # Remove system files and external dependencies
+            COMMAND ${LCOV} --remove ${CMAKE_BINARY_DIR}/coverage.info '/usr/*' '*/tests/*' --output-file ${CMAKE_BINARY_DIR}/coverage_filtered.info --ignore-errors unused,version
             # Generate HTML report
-            COMMAND ${GENHTML} --demangle-cpp -o ${CMAKE_BINARY_DIR}/coverage_report ${CMAKE_BINARY_DIR}/coverage.info
+            COMMAND ${GENHTML} --demangle-cpp --prefix ${CMAKE_SOURCE_DIR} -o ${CMAKE_BINARY_DIR}/coverage_report ${CMAKE_BINARY_DIR}/coverage_filtered.info
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS test
             COMMENT "Generating GCC coverage report"
